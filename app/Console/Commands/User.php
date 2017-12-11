64f2a1262f98bb3645f62dd3cb4233ea26b5ad03
kwdwkiss <kwdwkisscly@163.com>
2017-12-11 17:16:46 +0800

    数据迁移修复-1.13

--- app/Console/Commands/User.php
+++ app/Console/Commands/User.php
@@ -39,75 +39,80 @@ class User extends Command
      */
     public function handle()
     {
-        $startId = $this->argument('startId');
+        $startPage = $this->argument('page');
+        $startNum = $this->argument('num');
         $conn = \DB::connection('origin');
 
-        $startTime = time();
+        $total = $conn->table('ims_member')->count();
+        $pageSize = 1000;
+        $pageCount = ceil($total / $pageSize);
+        echo "total:$total pageSize:$pageSize pageCount:$pageCount\n";
+        for ($i = $startPage; $i < $pageCount; $i++) {
+            $rows = $conn->table('ims_member')->offset($i * $pageSize)->limit($pageSize)->get();
+            \DB::transaction(function () use ($startPage, $startNum, $i, $rows) {
+                if ($i == $startPage) {
+                    $num = $startNum;
+                } else {
+                    $num = 0;
+                }
+                foreach ($rows as $row) {
+                    $qq = trim($row->qq);
+                    $mobile = trim($row->mobile);
+                    $profileName = trim($row->username);
+                    $profileRemark = $row->admin_remark;
+                    $password = bcrypt('123456');
 
-        $conn->table('ims_member')
-            ->where('id', '>', $startId)
-            ->where('id', '<', 1000)
-            ->orderBy('id')
-            ->chunk(1000, function ($rows) use ($startTime) {
-                \DB::transaction(function () use ($rows, $startTime) {
-                    foreach ($rows as $row) {
-                        $qq = trim($row->qq);
-                        $mobile = trim($row->mobile);
-                        $profileName = trim($row->username);
-                        $profileRemark = $row->admin_remark;
-                        $password = bcrypt('123456');
-
-                        if (strpos($row->project, '删除') !== false) {
-                            $type = 401;
-                        } elseif ($row->project == '') {
-                            $type = 401;
-                        } elseif (strpos($row->project, '会员') !== false) {
-                            $type = 402;
-                        } elseif (strpos($row->project, '加群') !== false) {
-                            $type = 403;
-                        } else {
-                            continue;
-                        }
-
-                        if (!preg_match('/^1(3[0-9]|4[579]|5[0-35-9]|7[0-9]|8[0-9])\d{8}$/', $mobile)) {
-                            continue;
-                        }
-                        $user = \App\User::where('mobile', $mobile)->first();
-                        if ($user) {
-                            continue;
-                        }
-                        $user = \App\User::where('qq', $qq)->first();
-                        if ($user) {
-                            continue;
-                        }
-
-                        $updated_at = $created_at = Carbon::createFromTimestamp($row->create_time);
-
-                        $user = new \App\User();
-                        $user->timestamps = false;
-                        $user = $user->create([
-                            'type' => $type,
-                            'qq' => $qq,
-                            'mobile' => $mobile,
-                            'password' => $password,
-                            'created_at' => $created_at,
-                            'updated_at' => $updated_at
-                        ]);
-
-                        $userProfile = new UserProfile();
-                        $userProfile->timestamps = false;
-                        $userProfile->create([
-                            'user_id' => $user->id,
-                            'name' => $profileName,
-                            'remark' => $profileRemark,
-                            'created_at' => $created_at,
-                            'updated_at' => $updated_at
-                        ]);
-
-                        $useTime = time() - $startTime;
-                        echo $row->id . ' ' . $useTime . "\n";
+                    if (strpos($row->project, '删除') !== false) {
+                        $type = 401;
+                    } elseif ($row->project == '') {
+                        $type = 401;
+                    } elseif (strpos($row->project, '会员') !== false) {
+                        $type = 402;
+                    } elseif (strpos($row->project, '加群') !== false) {
+                        $type = 403;
+                    } else {
+                        continue;
                     }
-                });
+
+                    if (!preg_match('/^1(3[0-9]|4[579]|5[0-35-9]|7[0-9]|8[0-9])\d{8}$/', $mobile)) {
+                        continue;
+                    }
+                    $user = \App\User::where('mobile', $mobile)->first();
+                    if ($user) {
+                        continue;
+                    }
+                    $user = \App\User::where('qq', $qq)->first();
+                    if ($user) {
+                        continue;
+                    }
+
+                    $updated_at = $created_at = Carbon::createFromTimestamp($row->create_time);
+
+                    $user = new \App\User();
+                    $user->timestamps = false;
+                    $user = $user->create([
+                        'type' => $type,
+                        'qq' => $qq,
+                        'mobile' => $mobile,
+                        'password' => $password,
+                        'created_at' => $created_at,
+                        'updated_at' => $updated_at
+                    ]);
+
+                    $userProfile = new UserProfile();
+                    $userProfile->timestamps = false;
+                    $userProfile->create([
+                        'user_id' => $user->id,
+                        'name' => $profileName,
+                        'remark' => $profileRemark,
+                        'created_at' => $created_at,
+                        'updated_at' => $updated_at
+                    ]);
+
+                    echo "$i-$num $qq $mobile $profileName\n";
+                    $num++;
+                }
             });
+        }
     }
 }
