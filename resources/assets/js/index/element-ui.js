a95399b659d2d7eba5d9c65668ad524c865063b9
kwdwkiss <kwdwkisscly@163.com>
2017-12-30 17:29:32 +0800

    修复notification找不到问题

--- resources/assets/js/index/element-ui.js
+++ resources/assets/js/index/element-ui.js
@@ -12,5 +12,4 @@ Vue.prototype.$msgbox = MessageBox
 Vue.prototype.$alert = MessageBox.alert
 Vue.prototype.$confirm = MessageBox.confirm
 Vue.prototype.$prompt = MessageBox.prompt
-Vue.prototype.$notify = Notification
 Vue.prototype.$message = Message
