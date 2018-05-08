ff5b16526ef5f93a7be67611026c4943d820a513
kwdwkiss <kwdwkisscly@163.com>
2018-05-08 17:30:13 +0800

    优化用户后台管理创建和编辑功能

--- app/Http/Controllers/Admin/UserController.php
+++ app/Http/Controllers/Admin/UserController.php
@@ -50,41 +50,124 @@ class UserController extends Controller
     public function create()
     {
         \DB::transaction(function () {
-            $input = json_decode(request()->getContent(), true);
-            $request = new Request($input);
-            $this->validate($request, ['mobile' => 'required',]);
+            $type = request('type');
+            $mobile = request('mobile');
+            $qq = request('qq');
+            $wx = request('wx');
+            $ww = request('ww');
 
-            $type = array_get($input, 'type');
-            $mobile = trim(array_get($input, 'mobile'));
-            $qq = trim(array_get($input, 'qq'));
-            $wx = trim(array_get($input, 'wx'));
-            $ww = trim(array_get($input, 'ww'));
-
-            $name = trim(array_get($input, '_profile.name'));
-            $age = array_get($input, '_profile.age');
-            $gender = array_get($input, '_profile.gender');
-            $occupation = array_get($input, '_profile.occupation');
-            $province = array_get($input, '_profile.province');
-            $city = array_get($input, '_profile.city');
-            $remark = array_get($input, '_profile.remark');
-            $alipay = array_get($input, '_profile.alipay');
-
-            $tags = array_get($input, 'tags', []);
-
-            $user = new User();
-            $userProfile = new userProfile();
+            $name = request('_profile.name', '');
+            $age = request('_profile.age', '');
+            $gender = request('_profile.gender', 0);
+            $occupation = request('_profile.occupation', '');
+            $province = request('_profile.province', '');
+            $city = request('_profile.city', '');
+            $remark = request('_profile.remark', '');
+            $alipay = request('_profile.alipay', '');
 
             Taxonomy::where('pid', Taxonomy::USER_TYPE)->findOrFail($type);
-            $user->type = $type;
 
-            if (!preg_match('/^1(3[0-9]|4[579]|5[0-35-9]|7[0-9]|8[0-9])\d{8}$/', $mobile)) {
-                throw new JsonException('手机号错误');
+            if ($mobile) {
+                if (!preg_match('/^1(3[0-9]|4[579]|5[0-35-9]|7[0-9]|8[0-9])\d{8}$/', $mobile)) {
+                    throw new JsonException('手机号错误');
+                }
+                $exists = User::where('mobile', $mobile)->first();
+                if ($exists) {
+                    throw new JsonException('手机号码已存在');
+                }
+            } else {
+                throw new JsonException('手机号不能为空');
             }
-            $exists = User::where('mobile', $mobile)->first();
-            if ($exists && $exists->id != $user->id) {
-                throw new JsonException('手机号码已存在');
+
+            if ($qq) {
+                if (!preg_match('/^[1-9][0-9]{4,14}$/', $qq)) {
+                    throw new JsonException('QQ号错误');
+                }
+                $exists = User::where('qq', $qq)->first();
+                if ($exists) {
+                    throw new JsonException('QQ已存在');
+                }
+            } else {
+                $qq = null;
             }
-            $user->mobile = $mobile;
+
+            if ($wx) {
+                $exists = User::where('wx', $wx)->first();
+                if ($exists) {
+                    throw new JsonException('微信已存在');
+                }
+            } else {
+                $wx = null;
+            }
+
+            if ($ww) {
+                $exists = User::where('ww', $ww)->first();
+                if ($exists) {
+                    throw new JsonException('旺旺已存在');
+                }
+            } else {
+                $ww = null;
+            }
+
+            if (!in_array($gender, [0, 1, 2,])) {
+                throw new JsonException('性别错误');
+            }
+
+            $user = User::create([
+                'type' => $type,
+                'mobile' => $mobile,
+                'qq' => $qq,
+                'wx' => $wx,
+                'ww' => $ww,
+            ]);
+
+            UserProfile::create([
+                'user_id' => $user->id,
+                'name' => $name,
+                'age' => $age,
+                'gender' => $gender,
+                'occupation' => $occupation,
+                'province' => $province,
+                'city' => $city,
+                'alipay' => $alipay,
+                'remark' => $remark
+            ]);
+        });
+
+        return [];
+    }
+
+    public function update()
+    {
+        \DB::transaction(function () {
+            $type = request('type');
+            $mobile = request('mobile');
+            $qq = request('qq');
+            $wx = request('wx');
+            $ww = request('ww');
+
+            $name = request('_profile.name', '');
+            $age = request('_profile.age', '');
+            $gender = request('_profile.gender', 0);
+            $occupation = request('_profile.occupation', '');
+            $province = request('_profile.province', '');
+            $city = request('_profile.city', '');
+            $remark = request('_profile.remark', '');
+            $alipay = request('_profile.alipay', '');
+
+            $user = User::with('_profile')->findOrFail(request('id'));
+
+            Taxonomy::where('pid', Taxonomy::USER_TYPE)->findOrFail($type);
+
+            if ($mobile) {
+                if (!preg_match('/^1(3[0-9]|4[579]|5[0-35-9]|7[0-9]|8[0-9])\d{8}$/', $mobile)) {
+                    throw new JsonException('手机号错误');
+                }
+                $exists = User::where('mobile', $mobile)->first();
+                if ($exists && $exists->id != $user->id) {
+                    throw new JsonException('手机号码已存在');
+                }
+            }
 
             if ($qq) {
                 if (!preg_match('/^[1-9][0-9]{4,14}$/', $qq)) {
@@ -94,154 +177,50 @@ class UserController extends Controller
                 if ($exists && $exists->id != $user->id) {
                     throw new JsonException('QQ已存在');
                 }
-                $user->qq = $qq;
+            } else {
+                $qq = null;
             }
+
             if ($wx) {
                 $exists = User::where('wx', $wx)->first();
                 if ($exists && $exists->id != $user->id) {
                     throw new JsonException('微信已存在');
                 }
-                $user->wx = $wx;
+            } else {
+                $wx = null;
             }
+
             if ($ww) {
                 $exists = User::where('ww', $ww)->first();
                 if ($exists && $exists->id != $user->id) {
                     throw new JsonException('旺旺已存在');
                 }
-                $user->ww = $ww;
+            } else {
+                $ww = null;
             }
 
-            if ($name) {
-                $userProfile->name = $name;
-            }
-            if ($age) {
-                $userProfile->age = $age;
-            }
-            if ($gender) {
-                $userProfile->gender = $gender;
-            }
-            if ($occupation) {
-                $userProfile->occupation = $occupation;
-            }
-            if ($province) {
-                $userProfile->province = $province;
-            }
-            if ($city) {
-                $userProfile->city = $city;
-            }
-            if ($remark) {
-                $userProfile->remark = $remark;
-            }
-            if ($alipay) {
-                $userProfile->alipay = $alipay;
-            }
-            if ($tags) {
-                foreach ($tags as $tag_id) {
-                    Tag::findOrFail($tag_id);
-                }
+            if (!in_array($gender, [0, 1, 2,])) {
+                throw new JsonException('性别错误');
             }
 
-            $user->save();
-            $user->_profile()->save($userProfile);
-            $user->syncTag($tags);
-        });
+            $user->update([
+                'type' => $type,
+                'mobile' => $mobile,
+                'qq' => $qq,
+                'wx' => $wx,
+                'ww' => $ww,
+            ]);
 
-        return [];
-    }
-
-    public function update()
-    {
-        \DB::transaction(function () {
-            $input = json_decode(request()->getContent(), true);
-            $request = new Request($input);
-            $this->validate($request, ['mobile' => 'required',]);
-
-            $type = array_get($input, 'type');
-            $mobile = trim(array_get($input, 'mobile'));
-            $qq = trim(array_get($input, 'qq'));
-            $wx = trim(array_get($input, 'wx'));
-            $ww = trim(array_get($input, 'ww'));
-
-            $name = trim(array_get($input, '_profile.name'));
-            $age = array_get($input, '_profile.age');
-            $gender = array_get($input, '_profile.gender');
-            $occupation = array_get($input, '_profile.occupation');
-            $province = array_get($input, '_profile.province');
-            $city = array_get($input, '_profile.city');
-            $remark = array_get($input, '_profile.remark');
-            $alipay = array_get($input, '_profile.alipay');
-
-            $tags = array_get($input, 'tags', []);
-
-            $user = User::with('_profile')->findOrFail(request('id'));
-            $userProfile = $user->_profile;
-
-            Taxonomy::where('pid', Taxonomy::USER_TYPE)->findOrFail($type);
-            $user->type = $type;
-
-            if (!preg_match('/^1(3[0-9]|4[579]|5[0-35-9]|7[0-9]|8[0-9])\d{8}$/', $mobile)) {
-                throw new JsonException('手机号错误');
-            }
-            $exists = User::where('mobile', $mobile)->first();
-            if ($exists && $exists->id != $user->id) {
-                throw new JsonException('手机号码已存在');
-            }
-            $user->mobile = $mobile;
-
-            if ($qq && !preg_match('/^[1-9][0-9]{4,14}$/', $qq)) {
-                throw new JsonException('QQ号错误');
-            }
-            $exists = User::where('qq', $qq)->first();
-            if ($exists && $exists->id != $user->id) {
-                throw new JsonException('QQ已存在');
-            }
-            $user->qq = $qq;
-
-            $exists = User::where('wx', $wx)->first();
-            if ($exists && $exists->id != $user->id) {
-                throw new JsonException('微信已存在');
-            }
-            $user->wx = $wx;
-
-            $exists = User::where('ww', $ww)->first();
-            if ($exists && $exists->id != $user->id) {
-                throw new JsonException('旺旺已存在');
-            }
-            $user->ww = $ww;
-
-            if ($name) {
-                $userProfile->name = $name;
-            }
-            if ($age) {
-                $userProfile->age = $age;
-            }
-            if ($gender) {
-                $userProfile->gender = $gender;
-            }
-            if ($occupation) {
-                $userProfile->occupation = $occupation;
-            }
-            if ($province) {
-                $userProfile->province = $province;
-            }
-            if ($city) {
-                $userProfile->city = $city;
-            }
-            if ($remark) {
-                $userProfile->remark = $remark;
-            }
-            if ($alipay) {
-                $userProfile->alipay = $alipay;
-            }
-            if ($tags) {
-                foreach ($tags as $tag_id) {
-                    Tag::findOrFail($tag_id);
-                }
-            }
-
-            $user->save();
-            $user->syncTag($tags);
-            $userProfile->save();
+            $user->_profile->update([
+                'name' => $name,
+                'age' => $age,
+                'gender' => $gender,
+                'occupation' => $occupation,
+                'province' => $province,
+                'city' => $city,
+                'alipay' => $alipay,
+                'remark' => $remark
+            ]);
         });
 
         return [];
