cda47b30648af254a0d9ba8225ce348b37ad2e02
kwdwkiss <kwdwkisscly@163.com>
2018-05-29 23:27:26 +0800

    浏览器问题，淘气值显示不出来-1.3

--- resources/views/check_tb/page.blade.php
+++ resources/views/check_tb/page.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=900">
+    {{--<meta name="viewport" content="width=900">--}}
 
     <meta name="keywords" content="宏海网络 淘宝验号"/>
     <meta name="description" content="宏海网络 淘宝验号"/>
@@ -15,26 +15,26 @@
 
 </head>
 <body>
-<style>
-    .layer {
-        position: fixed;
-        top: 0;
-        bottom: 0;
-        left: 0;
-        right: 0;
-        background-image: url('/images/check.png');
-        background-color: #000;
-        opacity: 0.15;
-    }
+{{--<style>--}}
+    {{--.layer {--}}
+        {{--position: fixed;--}}
+        {{--top: 0;--}}
+        {{--bottom: 0;--}}
+        {{--left: 0;--}}
+        {{--right: 0;--}}
+        {{--background-image: url('/images/check.png');--}}
+        {{--background-color: #000;--}}
+        {{--opacity: 0.15;--}}
+    {{--}--}}
 
-    .timer {
-        position: fixed;
-        top: 110px;
-        right: 40px;
-        color: #000;
-        font-size: 30px;
-    }
-</style>
+    {{--.timer {--}}
+        {{--position: fixed;--}}
+        {{--top: 110px;--}}
+        {{--right: 40px;--}}
+        {{--color: #000;--}}
+        {{--font-size: 30px;--}}
+    {{--}--}}
+{{--</style>--}}
 {{--<div class="layer"></div>--}}
 {{--<div id="timer" class="timer"></div>--}}
 <iframe id="my-frame" src="{{$page}}" frameborder="0" style="width: 100%;min-height: 800px"></iframe>
