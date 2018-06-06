806ccedef77b3eda4c81633155c5ce1f1477cd1a
kwdwkiss <kwdwkisscly@163.com>
2018-06-07 01:36:26 +0800

    优化导航菜单显示

--- resources/assets/js/index/components/Nav.vue
+++ resources/assets/js/index/components/Nav.vue
@@ -6,10 +6,10 @@
                 <div class="navbar-header">
                     <button type="button" class="navbar-toggle collapsed" data-toggle="collapse"
                             data-target="#my-nav" aria-expanded="false">
-                        <span class="sr-only">Toggle navigation</span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
+                        <span style="color: #fff">菜单</span>
+                        <!--<span class="icon-bar"></span>-->
+                        <!--<span class="icon-bar"></span>-->
+                        <!--<span class="icon-bar"></span>-->
                     </button>
                     <a class="navbar-brand collapse-hide" href="javascript:" @click="index">宏海网络</a>
                 </div>
@@ -17,15 +17,15 @@
                 <!-- Collect the nav links, forms, and other content for toggling -->
                 <div class="collapse navbar-collapse" id="my-nav">
                     <ul class="nav navbar-nav">
-                        <li><a class="collapse-hide" href="javascript:" @click="index">账号查询</a>
                         <li class="dropdown">
                             <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button"
-                               aria-haspopup="true" aria-expanded="false">卖家工具<span
+                               aria-haspopup="true" aria-expanded="false">电商工具<span
                                     class="caret"></span></a>
                             <ul class="dropdown-menu">
+                                <li><a class="collapse-hide" href="javascript:" @click="index">恶意账号查询</a>
                                 <li><a class="collapse-hide" href="javascript:" @click="checkTb">淘宝验号</a></li>
                                 <li><a class="collapse-hide" href="javascript:" @click="checkPdd">拼多多验号</a></li>
-                                <li><a class="collapse-hide" href="javascript:" @click="searchOrder">查排名</a></li>
+                                <li><a class="collapse-hide" href="javascript:" @click="searchOrder">淘宝信用查询</a></li>
                             </ul>
                         </li>
                         <li class="dropdown">
@@ -38,7 +38,6 @@
                                 </li>
                             </ul>
                         </li>
-                        <li><a class="collapse-hide" href="javascript:" @click="customerService">联系客服</a></li>
                     </ul>
                     <ul class="nav navbar-nav navbar-right">
                         <template v-if="!user">
@@ -48,33 +47,39 @@
                         <template v-if="user">
                             <li class="dropdown">
                                 <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button"
-                                   aria-haspopup="true" aria-expanded="false">
-                                    积分:{{user._profile.amount}}<span class="caret"></span>
-                                </a>
+                                   aria-haspopup="true" aria-expanded="false">积分充值<span
+                                        class="caret"></span></a>
                                 <ul class="dropdown-menu">
-                                    <li><a class="collapse-hide" href="javascript:" @click="recharge">充值</a></li>
+                                    <li><a href="javascript:">积分:{{user._profile.amount}}</a></li>
+                                    <li role="separator" class="divider"></li>
+                                    <li><a class="collapse-hide" href="javascript:" @click="recharge">积分充值</a></li>
                                     <li><a class="collapse-hide" href="javascript:" @click="rechargeList">充值记录</a></li>
                                     <li><a class="collapse-hide" href="javascript:" @click="amountList">积分记录</a></li>
                                     <li><a class="collapse-hide" href="javascript:" @click="inviterLink">推广赚积分</a></li>
                                 </ul>
                             </li>
-                            <li><a class="collapse-hide" href="javascript:" @click="notificationList">通知
-                                <template v-if="unreadNotification>0">
-                                    （未读{{unreadNotification}}）
-                                </template>
-                            </a></li>
                             <li class="dropdown">
                                 <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button"
-                                   aria-haspopup="true" aria-expanded="false">{{user.mobile}} <span
+                                   aria-haspopup="true" aria-expanded="false">个人中心<span
                                         class="caret"></span></a>
                                 <ul class="dropdown-menu">
-                                    <!--<li role="separator" class="divider"></li>-->
+                                    <li><a href="javascript:">账号：{{user.mobile}}</a></li>
                                     <li><a class="collapse-hide" href="javascript:" @click="userModify">用户资料</a></li>
                                     <li><a class="collapse-hide" href="javascript:" @click="userMerchant">店铺资料</a></li>
+                                    <li role="separator" class="divider"></li>
+                                    <li>
+                                        <a class="collapse-hide" href="javascript:" @click="notificationList">
+                                            系统通知
+                                            <template v-if="unreadNotification>0">
+                                                （{{unreadNotification}}）
+                                            </template>
+                                        </a>
+                                    </li>
                                     <li><a class="collapse-hide" href="javascript:" @click="doLogout">退出</a></li>
                                 </ul>
                             </li>
                         </template>
+                        <li><a class="collapse-hide" href="javascript:" @click="customerService">联系客服</a></li>
                     </ul>
                 </div>
             </div>
@@ -169,4 +174,7 @@
 </script>
 
 <style scoped>
+    #my-nav {
+        max-height: 100%;
+    }
 </style>
