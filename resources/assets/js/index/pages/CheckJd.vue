49f221606a28f338155b6ab2fc44e09a7f140a0b
kwdwkiss <kwdwkisscly@163.com>
2018-06-13 23:43:54 +0800

    京东验号待测试和验收

--- resources/assets/js/index/pages/CheckJd.vue
+++ resources/assets/js/index/pages/CheckJd.vue
@@ -5,9 +5,12 @@
         </div>
         <div class="row">
             <p class="col-xs-offset-1 col-xs-10 text-danger">说明：用微信扫描下方二维码，依次点击下方链接，进入相应页面后截图保存即可。</p>
-            <p class="col-xs-offset-1 col-xs-10 text-danger">注意：验号前京东需要绑定微信</p>
+            <p class="col-xs-offset-1 col-xs-10 text-danger">注意：验号前需要登录京东或者绑定微信</p>
         </div>
         <div class="row">
+            <p class="col-xs-offset-1 col-xs-10">
+                <a href="https://plogin.m.jd.com/user/login.action">登录或绑定微信</a>
+            </p>
             <p class="col-xs-offset-1 col-xs-5">
                 <a href="/check_jd?page=jd_member">1、京东会员</a>
             </p>
