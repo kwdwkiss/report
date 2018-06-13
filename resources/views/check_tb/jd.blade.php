94afe603e5c00aa969e34f25e2ea692de71adf6d
kwdwkiss <kwdwkisscly@163.com>
2018-06-13 23:13:29 +0800

    基本完成京东验号-1.15

--- resources/views/check_tb/jd.blade.php
+++ resources/views/check_tb/jd.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width">
+    <meta name="viewport" content="width=device-width,user-scalable=0">
 
     <meta name="keywords" content="宏海网络 淘宝验号"/>
     <meta name="description" content="宏海网络 淘宝验号"/>
