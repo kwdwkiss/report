9d3319a3dcffc9491214307cda9337579c2f04e7
kwdwkiss <kwdwkisscly@163.com>
2018-07-02 18:23:09 +0800

    充值页面增加一个支付宝赏金红包图片

--- resources/assets/js/index/pages/Recharge.vue
+++ resources/assets/js/index/pages/Recharge.vue
@@ -3,30 +3,42 @@
         <div class="panel panel-default">
             <div class="panel-heading">积分充值</div>
             <div class="panel-body">
-                <div><h3>充值方式一：（推荐，不用写备注）</h3></div>
-                <iframe :src="pay_src" id="myiframe" scrolling="no" width="100%" height="350" frameborder="0"></iframe>
-                <div><h3>充值方式二：</h3></div>
-                <form class="form-horizontal" role="form">
-                    <div class="form-group">
-                        <label class="col-sm-3 control-label">充值说明</label>
-                        <div class="col-sm-9 form-control-static">
-                            <p>1、支付宝扫描下方二维码，输入需要充值的金额，最低充值1元。（1元=100积分）</p>
-                            <p>2、支付宝转款一定要备注手机号：<span class="text-danger">{{user.mobile}}</span>，并且只写手机号，否者系统无法识别</p>
-                            <p>3、完成支付宝转账后，请点击
-                                <a class="btn btn-success" @click="doRecharge" style="padding: 4px 10px">充值完成</a>
-                                ，或刷新页面积分即可到账</p>
-                            <p>4、若5分钟后积分未能到账，请联系充值客服，微信号：ywh171337832</p>
-                            <div class="col-xs-6">
-                                <img class="center-block" style="width:100%;max-width: 200px"
-                                     src="/images/recharge_img_demo.png" alt="">
-                            </div>
-                            <div class="col-xs-6">
-                                <img class="center-block" style="width:100%;max-width: 200px"
-                                     src="/images/tbpzw_alipay.png" alt="">
+                <div class="row"></div>
+                <div class="row">
+                    <div class="col-md-8">
+                        <div><h3>充值方式一：（推荐，不用写备注）</h3></div>
+                        <iframe :src="pay_src" id="myiframe" scrolling="no" width="100%" height="350"
+                                frameborder="0"></iframe>
+                    </div>
+                    <div class="col-md-4">
+                        <p class="text-center" style="color: red;font-size: 18px;font-weight: bold">支付宝扫码领红包福利</p>
+                        <img class="center-block" src="/images/alipay_redpack.png" alt="" style="max-width: 200px">
+                    </div>
+                </div>
+                <div class="row"><h3>充值方式二：</h3></div>
+                <div class="row">
+                    <form class="form-horizontal" role="form">
+                        <div class="form-group">
+                            <label class="col-sm-3 control-label">充值说明</label>
+                            <div class="col-sm-9 form-control-static">
+                                <p>1、支付宝扫描下方二维码，输入需要充值的金额，最低充值1元。（1元=100积分）</p>
+                                <p>2、支付宝转款一定要备注手机号：<span class="text-danger">{{user.mobile}}</span>，并且只写手机号，否者系统无法识别</p>
+                                <p>3、完成支付宝转账后，请点击
+                                    <a class="btn btn-success" @click="doRecharge" style="padding: 4px 10px">充值完成</a>
+                                    ，或刷新页面积分即可到账</p>
+                                <p>4、若5分钟后积分未能到账，请联系充值客服，微信号：ywh171337832</p>
+                                <div class="col-xs-6">
+                                    <img class="center-block" style="width:100%;max-width: 200px"
+                                         src="/images/recharge_img_demo.png" alt="">
+                                </div>
+                                <div class="col-xs-6">
+                                    <img class="center-block" style="width:100%;max-width: 200px"
+                                         src="/images/tbpzw_alipay.png" alt="">
+                                </div>
                             </div>
                         </div>
-                    </div>
-                </form>
+                    </form>
+                </div>
             </div>
         </div>
     </div>
