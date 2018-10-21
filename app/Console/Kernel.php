a4646987b6beb713478f3558e1b98cfc4f4a5d92
kwdwkiss <kwdwkisscly@163.com>
2018-10-21 14:44:42 +0800

    增加用户产品过期检查，用户认证过期检查-1.0

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -29,6 +29,9 @@ class Kernel extends ConsoleKernel
         $schedule->command('statement_day')->dailyAt('00:00');
         $schedule->command('statement_month')->monthlyOn(1, '00:01');
         $schedule->command('search_bill_month')->monthlyOn(1, '02:10');
+        //过期检查
+        $schedule->command('user_product:check')->dailyAt('01:00');
+        $schedule->command('user_auth:check')->dailyAt('01:01');
         //耗时任务2点执行
         $schedule->command('search_bill_day')->dailyAt('02:00');
         //清除temp文件
