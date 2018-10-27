a5a920084268c7ceec2c0ca213550f4d4a2d58d1
kwdwkiss <kwdwkisscly@163.com>
2018-10-27 18:59:24 +0800

    产品订单增加支付时间字段

--- database/migrations/2018_10_20_203449_create_product_bills_table.php
+++ database/migrations/2018_10_20_203449_create_product_bills_table.php
@@ -18,8 +18,9 @@ class CreateProductBillsTable extends Migration
             $table->integer('user_id');
             $table->integer('product_id');
             $table->integer('quantity');
-            $table->integer('pay_status');//0-未支付 1-已支付
             $table->integer('amount');
+            $table->integer('pay_status');//0-未支付 1-已支付
+            $table->timestamp('pay_at')->nullale();
             $table->timestamps();
         });
     }
