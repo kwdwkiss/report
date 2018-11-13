8ae819542e9261a6d494845e7c14852779470ba5
kwdwkiss <kwdwkisscly@163.com>
2018-11-13 10:12:28 +0800

    增加注册30天，充值3次，10元以上自动开通点赞功能，每天晚上定时脚本

--- Modules/Common/Entities/AccountFavor.php
+++ Modules/Common/Entities/AccountFavor.php
@@ -2,6 +2,7 @@
 
 namespace Modules\Common\Entities;
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Model;
 
 class AccountFavor extends Model
@@ -16,5 +17,35 @@ class AccountFavor extends Model
     public function _accountType()
     {
         return $this->hasOne(Taxonomy::class, 'id', 'account_type');
+    }
+
+    public static function autoEnable()
+    {
+        $userIds = RechargeBill::query()
+            ->whereHas('_user', function ($query) {
+                $query->doesntHave('_favor')
+                    ->where('created_at', '<', Carbon::now()->subMonth(1));
+            })
+            ->groupBy('user_id')
+            ->havingRaw('count(*)>?', [3])
+            ->havingRaw('sum(money)>?', [10])
+            ->pluck('user_id')->toArray();
+
+        $insertData = [];
+        foreach ($userIds as $userId) {
+            $insertData[] = [
+                'user_id' => $userId,
+                'admin_id' => 1,
+                'total' => 150,
+                'created_at' => Carbon::now()->toDateTimeString(),
+                'updated_at' => Carbon::now()->toDateTimeString(),
+            ];
+        }
+
+        $chunkData = array_chunk($insertData, 1000);
+        //批量插入数据
+        foreach ($chunkData as $insertItemData) {
+            UserFavor::query()->insert($insertItemData);
+        }
     }
 }
