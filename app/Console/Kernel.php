2f6efacd2048c9c44386a82af9fa9170302f61bc
kwdwkiss <kwdwkisscly@163.com>
2018-11-28 09:26:30 +0800

    定时脚本注释

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -31,15 +31,15 @@ class Kernel extends ConsoleKernel
         $schedule->command('statement_day')->dailyAt('00:00');
         $schedule->command('statement_month')->monthlyOn(1, '00:01');
 
-        $schedule->command('search_bill_day')->dailyAt('00:10');
-        $schedule->command('search_bill_month')->monthlyOn(1, '00:11');
+//        $schedule->command('search_bill_day')->dailyAt('00:10');
+//        $schedule->command('search_bill_month')->monthlyOn(1, '00:11');
 
-        $schedule->command('favor:restore')->monthlyOn(1, '00:20');
-        $schedule->command('favor:enable')->monthlyOn(1, '00:21');
+//        $schedule->command('favor:restore')->monthlyOn(1, '00:20');
+//        $schedule->command('favor:enable')->monthlyOn(1, '00:21');
 
         //过期检查
-        $schedule->command('user_product:check')->dailyAt('01:00');
-        $schedule->command('user_auth:check')->dailyAt('01:01');
+//        $schedule->command('user_product:check')->dailyAt('01:00');
+//        $schedule->command('user_auth:check')->dailyAt('01:01');
 
         //清除temp文件
         $schedule->command('clean_temp')->hourly();
